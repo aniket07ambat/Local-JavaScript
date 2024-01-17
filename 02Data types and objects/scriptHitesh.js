b1ef844ae048f4f96a7eca6aa03c55647c189340
aniket07ambat <ambataniket07gmail.com>
2024-01-17 16:31:56 +0530

    Praciticing code

---
+++ 02Data types and objects/scriptHitesh.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as newer version.
+
+// alert(3 + 3) // we are using node.js not browser.
+
+console.log(3 + 
+    2)    // console readibility should be high.
+
+console.log("Aniket")
+
+let name = "Aniket"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint for biggest number
+// string => ""
+// boolean => true/false
+//null => standloan value
+// undefined => 
+// symbol => unique
+
+// Object
+
+console.log(typeof state)
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
---
+++ Variables/scriptHitesh.js
@@ -0,0 +1,18 @@
+console.log("Study with Chai and Code");
+
+const accountId = 34534;
+let accountEmail = "Aniket@gmail.com";
+var accountPassward = "2345";
+accountCity = "Parbhani";
+let accountState;
+
+// accountId = 32453; Not allowed to change const variable.
+console.log(accountId); 
+
+accountEmail = "ani@gmail.com";
+accountPassward = "3423";
+accountCity = "Jaipur";
+
+console.table([accountId, accountEmail, accountPassward, accountCity, accountState]);
+
+console.log("Prefer not to use var because issue in block scope and functional scope.");
