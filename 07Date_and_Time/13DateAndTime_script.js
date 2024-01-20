a6d292062521b36d034520f9fe610a7a792eef91
aniket07ambat <ambataniket07gmail.com>
2024-01-20 10:54:55 +0530

    Rename files

---
+++ 07Date_and_Time/13DateAndTime_script.js
@@ -0,0 +1,21 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate)
+console.log(typeof myDate)
+
+console.log(myDate.toString())
+console.log(myDate.toJSON())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+
+let myCreatedDate1 = new Date(2023, 0, 8)
+console.log(myCreatedDate1.toDateString())
+
+let myCreatedDate2 = new Date(2022, 11, 7)
+console.log(myCreatedDate2.toDateString())
+console.log(myCreatedDate2.toLocaleString())
+
+
+
+
