79bd54b638bbb7486a8ded32d82534fb82a9d6ac
aniket07ambat <ambataniket07@gmail.com>
2024-02-02 19:02:35 +0530

    Learn

--- PRACTICE/Basics.js
+++ PRACTICE/Basics.js
@@ -83,6 +83,7 @@ console.log(array7.reverse());
 // para
 /////////// Functions
 
+/*
 function text(para1, para2 = 7) {
     return para1 + para2
 }
@@ -97,23 +98,131 @@ function text1(para1, para2) {
 
 console.log(text1("Aniket", "Ambat"));
 
-// Function Expression
+// Function Expression ?
+
+// Anonymous Function ?
+
+// Returning Object
+
+function text3(para1, para2) {
+    let obj = {
+        name: para1,
+        age: para2
+    }
+    return obj;
+}
+
+// optimised
+function text3(para1, para2) {
+    return {para1, para2};
+}
 
 
+console.log(text3("Aniket", 22));
+
+// Callback Function: I don't understand
+
+function text4(para1) {
+    console.log(para1);
+
+}
+function callBack(para1){
+    console.log(para1);
+}
+console.log(text4(callBack("Hell0")));
+
+// Arrow Functions ?
+
+function text(para1, para2 = 7) {
+    return para1 + para2
+}
+// function text5 (para1, para2) => {
+//     return para1 + para2
+// }
 
 
+// Resursive function
 
+function text6(number) {
+    if(number <= 0) {
+        return 1;
+    }
+    return text6(number - 1) * number
+}
 
+// Optimesed
 
+function text9(number) {
+    return number === 0 ? 1 : n * text9(number - 1)
+}
 
+console.log(text6(3));
+ */
 
+/////////// OBJECTS
 
+const obj1 = {
+    "name A": 'John',
+    age: 25 
+}
+console.log(obj1["name A"]);
 
+obj1.city = "New York"
+obj1.age = 45
+console.log(obj1);
 
+const obj2 = { a: 1, b: 2 } 
+const obj3 = { b: 2, a: 1 }
+console.log(obj2 == obj3); //  ?
 
+const obj4 = {
+    "name A": 'John',
+    age: 25,
+    address: {
+        city: "Parbhani",
+        pin: 43165
+    },
+    greet: () =>{
+        return greeting;   
+    }
+    
+}
+ ////// What is happening in Object Method
 
+// console.log(obj4);
+// // console.log(obj4.greet());
+// console.log(obj4.greet); 
+// const obj5 = obj4.greet
+// console.log(obj5);
 
+delete obj4.age
+console.log(obj4);
 
+const obj6 = { x: 10, y: 20, z: 30 }
+for (const key in obj6) {
+   console.log(`${key}: ${obj6[key]}`);
+    
+}
+
+//// Object Destructuring:
+
+// Object from Arrays
+
+const array1 = ['name', 'age']
+const array2 = ['John', 25]
+
+function createObj1(para1, para2) {
+    const length = para1.length
+    let obj7 = {}
+    for (let index = 0; index < length; index++) {
+        // obj7.para1[index] = para2[index] // What is problem....
+        console.log(`${para1[index]}: ${para2[index]}`);
+    }
+    return obj7
+}
+
+const obj8 = createObj1(array1, array2)
+console.log(obj8);
 
 
 
---
+++ PRACTICE/Basics2.js
@@ -0,0 +1,27 @@
+let concatenatedArray = ['a', 'b'].concat(['c', 'd']);
+console.log(concatenatedArray);
+
+// array.sort( content Find what it is?)
+
+// Function Expression ?
+
+// Anonymous Function ?
+
+// Callback Function: I don't understand
+
+let isEqual = JSON.stringify({ a: 1, b: 2 }) === JSON.stringify({ b: 2, a: 1 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
